795156657319235ac9bed424ce682ad9784d9606
Kushal Dhungana <k.dhungana@jacobs-university.de>
2020-02-22 19:54:42 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useEffect,useState} from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const[time, setTime] = useState(new Date().getSeconds());
+    const [print, setPrint] = useState("");
+
+    useEffect(() => {
+      const timer = setTimeout(() => {
+        setTime(new Date().getSeconds());
+      }, 1000);
+      fizzBuzz(time);
+      return () => {
+        clearTimeout(timer);
+      };
+    }, [time]);
+
+    function fizzBuzz(time) {
+      if (time % 3 === 0) {
+        setPrint("Fizz");
+      } else {
+        setPrint("");
+      }
+      if (time % 5 === 0) {
+        setPrint("Buzz");
+      }
+      if (time % 3 === 0 && time % 5 === 0) {
+        setPrint("FizzBuzz");
+      }
+    }
+    return (
+      <div className="App">
+        <h1>Fizz Buzz Clock</h1>
+        <h2>Seconds: {time} </h2>
+        <h2>{print}</h2>
+      </div>
+    );
 }
 
 export default App;
